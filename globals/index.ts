2a4cb4703595557b9d69095bb839e888e739a7de
Kasim Taskin <kasimt@kth.se>
2020-04-12 22:37:36 +0200

    Use library functions

--- globals/index.ts
+++ globals/index.ts
@@ -3,5 +3,5 @@ import { MapPlayer } from "../handles/player";
 export const Players: MapPlayer[] = [];
 
 for (let i = 0; i < bj_MAX_PLAYER_SLOTS; i++) {
-  Players[i] = MapPlayer.fromHandle(Player(i));
+  Players[i] = MapPlayer.fromIndex(i);
 }
